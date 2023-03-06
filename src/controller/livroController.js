362808a82c0597d0f34f3647014c2f7f132237ab
marcos <marcosaureliofarias2023@gmail.com>
2023-03-05 21:36:01 -0300

    feature relacionamento

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -2,9 +2,12 @@ import livros from "../models/Livro.js";
 
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.status(200).json(livros);
-    });
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.status(200).json(livros);
+      });
   };
 
   static cadastrarLivro = (req, res) => {
@@ -24,15 +27,18 @@ class LivroController {
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
 
-    livros.findById(id, (err, livros) => {
-      if (err) {
-        res
-          .status(400)
-          .send({ message: `${err.message} - Id do livro não localizado.` });
-      } else {
-        res.status(200).send(livros);
-      }
-    });
+    livros
+      .findById(id)
+      .populate("autor", "nome")
+      .exec((err, livros) => {
+        if (err) {
+          res
+            .status(400)
+            .send({ message: `${err.message} - Id do livro não localizado.` });
+        } else {
+          res.status(200).send(livros);
+        }
+      });
   };
 
   static atualizarLivro = (req, res) => {
@@ -56,6 +62,14 @@ class LivroController {
       } else {
         res.status(500).send({ message: err.message });
       }
+    });
+  };
+
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
+
+    livros.find({ editora: editora }, {}, (err, livros) => {
+      res.status(200).send(livros);
     });
   };
 }
